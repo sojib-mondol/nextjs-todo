08efefa3c684559e35ace29824bf23351d48c04d
sojib-sudo <mdsojibmandol@gmail.com>
2023-07-30 07:13:00 -0600

    footer added

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='w-full'>
+            <p className='text-center text-2xl font-medium '>
+                &copy; 2023 Todo™. All Rights Reserved.
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
