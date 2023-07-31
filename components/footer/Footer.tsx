9aa45e9a2cfa1cf56415bc2788d0459ee4dc2f9c
sojib-sudo <mdsojibmandol@gmail.com>
2023-07-31 16:14:32 +0530

    code merged

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='w-full'>
+            <p className='text-center  '>
+                &copy; 2023 Todo™. All Rights Reserved. 
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ pages/homePage/HomePage.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="h-screen">
+        <p className="text-center text-xl"> Add a tasks to todo list</p>
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
